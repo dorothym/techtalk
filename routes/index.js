021e1432dc4cda4da1c82d4a8a788f72870b32f5
dorothym <dorothy.v.moore@gmail.com>
2016-03-27 22:45:43 -0400

    set up CORS response headers

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CORS test', para: 'Source application' });
 });
+
+router.get('/data', function(req, res, next) {
+  res.render('data');
+});
+
+router.get('/shared-data',function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.render('shared-data')
+});
+
+
 
 module.exports = router;
